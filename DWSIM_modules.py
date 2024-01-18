2e2fd9b4da395bbb524aae7845a8714b1aea7797
Alex <55703624+AleSteB@users.noreply.github.com>
2024-01-18 19:09:53 +0100

    Update DWSIM_modules.py

--- DWSIM_modules.py
+++ DWSIM_modules.py
@@ -18,4 +18,25 @@ from System.IO import Directory, Path, File
 from System import String, Environment
 from System.Collections.Generic import Dictionary
 
-import EnzymeMLtoDWSIM-modules.py
+import ELNs_to_KG_modules
+
+# Path to DWSIM-Directory
+dwsimpath = os.getenv('LOCALAPPDATA') + "\\DWSIM8\\"
+
+clr.AddReference(dwsimpath + "DWSIM")
+clr.AddReference(dwsimpath + "CapeOpen.dll")
+clr.AddReference(dwsimpath + "DWSIM.Automation.dll")
+clr.AddReference(dwsimpath + "DWSIM.Interfaces.dll")
+clr.AddReference(dwsimpath + "DWSIM.GlobalSettings.dll")
+clr.AddReference(dwsimpath + "DWSIM.SharedClasses.dll")
+clr.AddReference(dwsimpath + "DWSIM.Thermodynamics.dll")
+clr.AddReference(dwsimpath + "DWSIM.UnitOperations.dll")
+clr.AddReference(dwsimpath + "DWSIM.Inspector.dll")
+clr.AddReference(dwsimpath + "System.Buffers.dll")
+clr.AddReference(dwsimpath + "DWSIM.MathOps.dll")
+clr.AddReference(dwsimpath + "TcpComm.dll")
+clr.AddReference(dwsimpath + "Microsoft.ServiceBus.dll")
+clr.AddReference(dwsimpath + "DWSIM.FlowsheetSolver.dll")
+clr.AddReference("System.Core")
+clr.AddReference("System.Windows.Forms")
+clr.AddReference(dwsimpath + "Newtonsoft.Json")
