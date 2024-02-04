33c8ddcc431b16040f15ca1243a4b29d3c44cc75
AlexB <55703624+AleSteB@users.noreply.github.com>
2024-02-04 13:15:09 +0100

    Update GCA_injection.py

--- GCA_injection.py
+++ GCA_injection.py
@@ -9,12 +9,13 @@ from owlready2 import *
 
 onto = get_ontology("./ontologies/KG-DWSIM_EnzML_ELN_output.owl").load()
 
+cat_name = "Laccase"
 
 with onto:
 #(hasReactionComponent some ({Sub_Laccase})) and (isPotentiallyCatalysedBy some ({Sub_Laccase}) SubClassOf hasCatalyst some ({Sub_Laccase})
-    cat_sub_indv = onto.search_one(label = subst)
-    gca = GeneralClassAxiom(onto.hasReactionComponent.some() & onto.has_location.some(onto.Heart)) # Left side
-    gca.is_a.append(onto.CardiacDisorder) # Right side
+    cat_sub_indv = onto.search_one(label = cat_name).instances().first()
+    gca = GeneralClassAxiom(onto.hasReactionComponent.some(cat_sub_indv) & onto.isPotentiallyCatalysedBy.some(cat_sub_indv)) # Left side
+    gca.is_a.append(onto.hasCatalyst.some(cat_sub_indv)) # Right side
       
 #'Laccase Reaction' and (hasCatalyst some ((material entity or chemical entity) and (inverse (hasReactionComponent some 'Laccase Reaction'))) SubClassOf 'Catalysed Laccase Reaction'
 #(hasEductComponent some ABTS_red) and (hasEductComponent some Oxygen) and (hasProductComponent some ABTS_ox) SubClassOf hasReactionRole some ({Laccase_reaction_indv})
