4162c93718e75c598eb1ff8340dbf1c3d80d9a70
AlexB <55703624+AleSteB@users.noreply.github.com>
2024-10-04 15:48:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ Created on Thu Jan 18 19:35:20 2024
 
 import ELNs_to_KG_modules
 import DWSIM_modules
-import individual_extr
-import robot_KG_shrinker
+#import individual_extr
+#import robot_KG_shrinker
 
-base_onto_path = "./ontologies/BaseOntology.owl"
+#base_onto_path = "./ontologies/BaseOntology.owl"
+#BaseOntology_for_CSTR.owl
+base_onto_path = "./ontologies/BaseOntology_for_CSTR.owl"
 
+#enzml_XLSX_path = "./ELNs/EnzymeML_Template_18-8-2021_KR.xlsm"
+enzml_XLSX_path = "./ELNs/EnzymeML_Template_Sain.xlsm"
 
-enzml_XLSX_path = "./ELNs/EnzymeML_Template_18-8-2021_KR.xlsm"
-pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1.xlsx"
+#pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1.xlsx"
+pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1_Sain_AB.xlsx"
 
-extended_ontology_path ="./ontologies/KG-DWSIM_EnzML_ELN.owl"
-filename_DWSIM ="ABTS_ox.dwxmz"
+extended_ontology_path ="./ontologies/KG-DWSIM_CSTR_ELN.owl"
+filename_DWSIM ="Sain.dwxmz"
 
 ##
 # Loading the information stored in both enz_str and eln_str Excel-files
@@ -31,6 +35,5 @@ PFD_uuid = ELNs_to_KG_modules.run(enzml_XLSX_path,pfd_XLSX_path, base_onto_path,
 DWSIM_modules.run(filename_DWSIM,PFD_uuid,extended_ontology_path)
 
 
-
-individual_extr.run()
-robot_KG_shrinker.run()
+#individual_extr.run()
+#robot_KG_shrinker.run()
