85756c4728fae78e06b96f0b58bf67f9d9075565
AlexB <55703624+AleSteB@users.noreply.github.com>
2024-10-25 09:23:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ import DWSIM_modules
 #import individual_extr
 #import robot_KG_shrinker
 
-#base_onto_path = "./ontologies/BaseOntology.owl"
+base_onto_path = "./ontologies/BaseOntology.owl"
 #BaseOntology_for_CSTR.owl
-base_onto_path = "./ontologies/BaseOntology_for_CSTR.owl"
+#base_onto_path = "./ontologies/BaseOntology_for_CSTR.owl"
 
-#enzml_XLSX_path = "./ELNs/EnzymeML_Template_18-8-2021_KR.xlsm"
-enzml_XLSX_path = "./ELNs/EnzymeML_Template_Sain.xlsm"
+enzml_XLSX_path = "./ELNs/EnzymeML_Template_18-8-2021_KR.xlsm"
+#enzml_XLSX_path = "./ELNs/EnzymeML_Template_Sain.xlsm"
 
-#pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1.xlsx"
-pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1_Sain_AB.xlsx"
+pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1.xlsx"
+#pfd_XLSX_path = "./ELNs/New-ELN_Kinetik_1_Sain_AB.xlsx"
 
 extended_ontology_path ="./ontologies/KG-DWSIM_CSTR_ELN.owl"
 filename_DWSIM ="Sain.dwxmz"
